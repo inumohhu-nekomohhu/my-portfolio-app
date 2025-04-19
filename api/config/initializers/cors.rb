a94f8aa52bb893727c853b0d0ecfc24ce356343d
inumohu <syanowerl@gmail.com>
2025-04-20 03:44:22 +0900

    Add ALB health check route

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -2,7 +2,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'http://localhost:5173', 'http://my-portfolio-frontend-20250419.s3-website-ap-northeast-1.amazonaws.com', 'https://api.linuxstudy5678.com'
-  
+
       resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
