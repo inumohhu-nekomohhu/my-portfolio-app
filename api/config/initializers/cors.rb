ad5712dbebc3d0479103ead219479857d9d7dcff
inumohu <syanowerl@gmail.com>
2025-04-20 05:27:49 +0900

    Fix CORS settings

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -6,6 +6,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
+          expose: ['access-token', 'expiry', 'token-type', 'uid', 'client', 'Authorization'],
           credentials: true
     end
   end
